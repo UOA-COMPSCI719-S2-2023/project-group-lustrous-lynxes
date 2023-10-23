8ec81e5bd22d969e8f4aaa1052c03bc54e3c2e43
jason <jason_3100@outlook.com>
2023-10-24 09:56:51 +1300

    Main page Login form with css

--- routes/application-routes.js
+++ routes/application-routes.js
@@ -5,7 +5,7 @@ const testDao = require("../modules/test-dao.js");
 
 router.get("/", async function(req, res) {
 
-    res.locals.title = "My route title!";
+    res.locals.title = "Lustrous Lynxes";
     res.locals.allTestData = await testDao.retrieveAllTestData();
 
     res.render("home");
