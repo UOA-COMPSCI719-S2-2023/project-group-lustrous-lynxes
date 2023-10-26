3a5d7a022987ab1083372cbc973866b76d8718bd
Richard Craig <rcraig9891@gmail.com>
2023-10-26 15:20:18 +1300

    Condense Code for require

--- routes/application-routes.js
+++ routes/application-routes.js
@@ -2,26 +2,26 @@ const express = require("express");
 const router = express.Router();
 
 
-const {checkLoginCredentials, verifyAuthenticated,removeToken,checkIfLoggedIn} = require("../middleware/auth-middleware.js");
+const authUser = require("../middleware/auth-middleware.js");
 
 //render home page. User remains logged in until logged out
-router.get("/", verifyAuthenticated, (req, res) => {
+router.get("/", authUser.verifyAuthenticated, (req, res) => {
     res.locals.title = "Lustrous Lynxes";
     res.render("account");
 });
 
 //Login Clicked
-router.get("/login",checkIfLoggedIn, (req, res) => {
+router.get("/login",authUser.checkIfLoggedIn, (req, res) => {
     res.render("login");
 });
 
 //Submit Login form. Verification done first. If correct, call next and proceed with code.
-router.post("/login", checkLoginCredentials, (req, res) => {
+router.post("/login", authUser.checkLoginCredentials, (req, res) => {
     res.render("account")
 });
 
 //Logout Clicked
-router.get("/logout",removeToken, (req, res) => {
+router.get("/logout",authUser.removeToken, (req, res) => {
     res.clearCookie("authToken");
     res.locals.user = null;
     res.setToastMessage("Successfully logged out!");
