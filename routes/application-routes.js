6398f7ee7261aebcefdae28d4f0cb2f404c80227
syan797 <syan797@aucklanduni.ac.nz>
2023-11-05 13:22:53 +1300

    formatting

--- routes/application-routes.js
+++ routes/application-routes.js
@@ -375,14 +375,14 @@ router.get("/full-article", async (req, res) => {
             allArticleComments[i].likes = likes;
             //Only run this if user is logged in.
             if (res.locals.user){
-            const likeByUser = await commentDao.checkLikeByCurrentUser(res.locals.user.id, allArticleComments[i].id);
-            //If the user has already liked the comment, then disable the ability to like that comment.
-            if(likeByUser){
-                allArticleComments[i].enableUserLike = false;
-            }else{
-                allArticleComments[i].enableUserLike = true;
+                const likeByUser = await commentDao.checkLikeByCurrentUser(res.locals.user.id, allArticleComments[i].id);
+                //If the user has already liked the comment, then disable the ability to like that comment.
+                if(likeByUser){
+                    allArticleComments[i].enableUserLike = false;
+                }else{
+                    allArticleComments[i].enableUserLike = true;
+                }
             }
-        }
         }
         //Sort Comments by likes.
         allArticleComments.sort((a,b) =>{
